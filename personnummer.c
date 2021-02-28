e07b50f3af685530a2b47848cd013136f2a56888
abrarhussainshahid <abrar.hussain-shahid@yaelev.se>
2021-02-28 14:49:09 +0100

    checking valid personnummer

--- personnummer.c
+++ personnummer.c
@@ -20,8 +20,6 @@ int main()
 label:
     printf("Enter personnummer: ");
     gets(personNummer);
-    //printf("\nFormat is ok: %s and Length: %zu", personNummer, strlen(personNummer));
-    //printf("\nLength: %zu", strlen(personNummer));
 
     //check format 811112-4321 and length
     for (int i = 0; i < strlen(personNummer); ++i)
@@ -33,14 +31,8 @@ label:
         }
     }
     printf("Personnummer format correct: %s\n", personNummer);
+
     //Format is ok. Remove now '-' to string 8111124321
-    /*for (int i = 0; i < strlen(personNummer); ++i)
-    {
-        if (personNummer[i] = '-')
-        {
-            personNummer[i] = ' ';
-        }
-    }*/
     for (int i = 0, j = 0; personNummer[i] != '\0'; ++i)
     {
         if ((personNummer[i] >= '0' && personNummer[i] <= '9'))
@@ -51,8 +43,33 @@ label:
 
         personNummerWithoutDash[j] = '\0';
     }
-    printf("Personnummer without dash '-': ");
+    printf("Dash '-' removed from personnummer: ");
     puts(personNummerWithoutDash);
+
+    //Checking personnummer is valid using Luhn algorithm
+    int numberSum = 0, isSecondNumber = 0; //0 = false, 1 = true
+
+    for (int i = strlen(personNummerWithoutDash) - 1; i >= 0; i--)
+    {
+        int digit = personNummerWithoutDash[i] - '0'; //convert char to integer
+
+        if (isSecondNumber == 1)
+            digit = digit * 2;
+
+        // We add two digits to handle cases that make two digits after doubling
+        numberSum += digit / 10;
+        numberSum += digit % 10;
+
+        isSecondNumber = !isSecondNumber;
+    }
+    if ((numberSum % 10 == 0))
+    {
+        printf("Personnummer is valid");
+    }
+    else
+    {
+        printf("Personnummer is not valid");
+    }
 
     return 0;
 }
